d4a3f3ff3be457893cd3585100f02c6ce275dacb
oksana <asterisk.hu@gmail.com>
2020-05-11 22:47:48 +0300

    upadate

--- ProductMicroservice/Startup.cs
+++ ProductMicroservice/Startup.cs
@@ -1,6 +1,8 @@
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Reflection;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
@@ -10,6 +12,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.OpenApi.Models;
 using ProductMicroservice.DBContexts;
 using ProductMicroservice.Repository;
 
@@ -17,20 +20,33 @@ namespace ProductMicroservice
 {
     public class Startup
     {
-        public Startup(IConfiguration configuration)
+        private readonly string AllowSpecificOrigins = "_allowSpecificOrigins";
+        public Startup(IConfiguration _configuration)
         {
-            Configuration = configuration;
+            this._configuration = _configuration;
         }
 
-        public IConfiguration Configuration { get; }
+        public IConfiguration _configuration { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddCors(options =>
+            {
+                options.AddPolicy(AllowSpecificOrigins,
+                    builder =>
+                    {
+                        builder.WithOrigins("*")
+                        .AllowAnyHeader()
+                        .AllowAnyMethod();
+                    });
+            });
+
             services.AddControllers();
-            services.AddDbContext<ProductContext>(o => o.UseSqlServer(Configuration.GetConnectionString("ProductDB")));
-            services.AddTransient<IProductRepository, ProductRepository>();
-            
+            services.AddDbContext<UserContext>(o => o.UseSqlServer(_configuration.GetConnectionString("UserDB")));
+            services.AddTransient<IUserRepository, UserRepository>();
+            ConfigureSwagger(services);
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -41,6 +57,18 @@ namespace ProductMicroservice
                 app.UseDeveloperExceptionPage();
             }
 
+            var swaggerRoutePrefix = _configuration["Swagger:RoutePrefix"];
+
+            app.UseCors(AllowSpecificOrigins);
+
+            app.UseSwagger();
+
+            app.UseSwaggerUI(options =>
+            {
+                options.SwaggerEndpoint("/swagger/v1/swagger.json", "Swagger Demo API");
+                options.RoutePrefix = swaggerRoutePrefix.Trim(char.Parse("/"));
+            });
+
             app.UseRouting();
 
             app.UseAuthorization();
@@ -48,6 +76,23 @@ namespace ProductMicroservice
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllers();
+            });
+        }
+
+        private static void ConfigureSwagger(IServiceCollection services)
+        {
+            services.AddSwaggerGen(swagger =>
+            {
+                swagger.SwaggerDoc("v1",
+                    new OpenApiInfo
+                    {
+                        Title = "Swagger Service API",
+                        Version = "v1"
+                    });
+
+                var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+                var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
+                swagger.IncludeXmlComments(xmlPath);
             });
         }
     }
